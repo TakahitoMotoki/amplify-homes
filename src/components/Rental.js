7dd3ab39ee61058a233fa113f6dde90519d19300
TakahitoMotoki <t.motoki.1030@gmail.com>
2022-07-23 11:26:17 +0900

    Modified Rental page

--- src/components/Rental.js
+++ src/components/Rental.js
@@ -1,5 +1,4 @@
-import { Button, Flex, Heading, SelectField, ScrollView } from "@aws-amplify/ui-react";
-import { Table, TableCell, TableBody, TableHead, TableRow } from "@aws-amplify/ui-react";
+import { Button, Card, Flex, Heading, Image, SelectField, Text } from "@aws-amplify/ui-react";
 import { API } from "aws-amplify";
 import { useState, useEffect } from 'react';
 
@@ -8,30 +7,17 @@ import './Rental.css';
 const apiName = 'api0c080238';
 const apiPath = '/farms/avail';
 
-function Record(props) {
-   return(
-      <TableRow onClick={() => alert()} >
-         <TableCell>{props.displayData.name}</TableCell>
-         <TableCell>{props.displayData.is_organic}</TableCell>
-         <TableCell>{props.displayData.avail_number}</TableCell>
-         <TableCell>{props.temperature}</TableCell>
-         <TableCell>{props.precipitation}</TableCell>
-      </TableRow>
-   )
-}
-
 function CustomSelectField(props) {
    return(
-      <Flex direction="row" alignItems="center" margin="20px 0px 0px 0px">
          <SelectField
             label={props.title}
             size='small'
+            margin={props.margin}
+            width={props.width}
             variation='quiet'
             placeholder='-- 選択してください --'
-            width="280px"
             options={props.options}
          ></SelectField>
-      </Flex>
    )
 }
 
@@ -45,11 +31,15 @@ function Search(props) {
          <Flex direction="row" justifyContent="space-between">
             <CustomSelectField 
                title="農作物" 
+               margin="20px 0 0 0"
+               width="40%"
                options={["エダマメ", "ビーツ"]}
             />
 
             <CustomSelectField
                title="平均気温"
+               margin="20px 0 0 0"
+               width="40%"
                options={["寒冷（10℃未満）", "冷涼（10℃~15℃）", "温暖（20℃~25℃）", "熱帯（25℃以上）"]} 
             />
          </Flex>
@@ -57,11 +47,15 @@ function Search(props) {
          <Flex direction="row" justifyContent="space-between">
             <CustomSelectField
                title="降水量"
+               margin="20px 0 0 0"
+               width="40%"
                options={["少なめ", "普通", "多め"]} 
             />
 
             <CustomSelectField
                title="有機栽培"
+               margin="20px 0 0 0"
+               width="40%"
                options={["可", "不可"]} 
             />
          </Flex>
@@ -70,7 +64,7 @@ function Search(props) {
             <Button
                variation='primary'
                isLoading={false}
-               width="100%"
+               width="60%"
                margin='40px 0px 0px 0px'
             >
                検索
@@ -80,69 +74,80 @@ function Search(props) {
    )
 }
 
+function Record(props) {
+   const organic_converter = {0: "不可", 1: "可"}
+   return(
+      <Card
+         width="100%"
+         border="solid gray"
+         borderRadius="10px"
+         margin="20px"
+      >
+         <Flex direction="row" alignItems="flex-start">
+            <Image
+               alt="Road to milford sound"
+               src="/road-to-milford-new-zealand-800w.jpg"
+               width="30%"
+            />
+            <Flex
+               direction="column"
+               alignItems="flex-start"
+               width="70%"
+            >
+               <Heading level={5}>
+                  {props.displayData.name}
+               </Heading>
+
+               <Text>
+                  有機栽培: {organic_converter[props.displayData.is_organic]}
+               </Text>
+
+               <Text>
+                  空き農園： {props.displayData.avail_number} / {props.displayData.farm_number}
+               </Text>
+               <CustomSelectField
+                  title="農園の選択"
+                  margin="0"
+                  options={props.displayData.avail_farms}
+               />
+
+               <Button
+                  size='small'
+                  margin="20px 0 0 0"
+                  width="100%"
+               >
+                  レンタル
+               </Button>
+            </Flex>
+         </Flex>
+      </Card>
+   )
+}
+
+function DataList(props) {
+   const displayData = props.displayData;
+   return(
+      <div>
+         <Heading level={4} margin="0 0 30px 0">
+            検索結果
+         </Heading>
+
+         {displayData.map(displayData => (
+            <Record
+               displayData={displayData}
+            />
+         ))}
+      </div>
+   )
+
+}
+
 function Result(props) {
    const displayData = props.displayData;
    return(
       <div class='result-container'>
          <Search />
-
-         <Heading level={4} margin="0 0 30px 0">
-            検索結果
-         </Heading>
-         
-         <Table
-            highlightOnHover={true}
-            size={undefined}
-            variation={undefined}
-         >
-            <TableHead>
-               <TableRow>
-                  <TableCell as="th">農園</TableCell>
-                  <TableCell as="th">有機栽培</TableCell>
-                  <TableCell as="th">空き農園数</TableCell>
-                  <TableCell as="th">気温</TableCell>
-                  <TableCell as="th">降水量</TableCell>
-               </TableRow>
-            </TableHead>
-            <TableBody>
-               {displayData.map(displayData => (
-                  <Record
-                     displayData={displayData}
-                     temperature="温暖"
-                     precipitation="湿潤"
-                  />
-               ))}
-            </TableBody>
-         </Table>
-      </div>
-   )
-}
-
-function ResultDetail(props) {
-   return(
-      <div class='resultdetail-container'>
-         <ScrollView
-            position="fixed"
-            orientation="horizontal"
-            width="100%"
-            height="100vh"
-            padding="50px 15px 0 15px"
-         >
-            <Heading level={4} color="#DDD">
-               詳細データ
-            </Heading>
-            <p>
-            ・写真
-            ・平均気温
-            ・平均湿度
-            ・平均光量（快晴の割合とか）
-            ・降水量
-            ・土壌成分履歴
-            ・作付の履歴（連作障害とかチェック用）
-            ・作付可能な農作物
-            ・アラート（連作障害起こる可能性あり、XXの害虫に注意など）
-            </p>
-         </ScrollView>
+         <DataList displayData={displayData} />
       </div>
    )
 }
@@ -156,7 +161,14 @@ export default function Rental(props) {
                            width: 0.7,
                            area: 3.5,
                            is_organic: 1,
-                           avail_number: "24 / 50",
+                           avail_number: 3,
+                           avail_farms:
+                              [
+                                 "農場003 （1,500円/月、最終利用: 2021/12/17）",
+                                 "農場007 （2,500円/月、最終利用: 2022/06/17）",
+                                 "農場019 （2,500円/月、最終利用: 2022/06/17）",
+                              ],
+                           farm_number: 50,
                            temperature: { Aug: 23.0, Sep: 18.5, Oct: 16.8 },
                            temperature: { Aug: 23.0, Sep: 18.5, Oct: 16.8 },
                            humidity: { Aug: 23.0, Sep: 18.5, Oct: 16.8 },
@@ -169,7 +181,14 @@ export default function Rental(props) {
                            width: 0.7,
                            area: 3.5,
                            is_organic: 0,
-                           avail_number: "8 / 100",
+                           avail_number: 3,
+                           avail_farms:
+                              [
+                                 "農場003 （1,500円/月、最終利用: 2021/12/17）",
+                                 "農場007 （2,500円/月、最終利用: 2022/06/17）",
+                                 "農場019 （2,500円/月、最終利用: 2022/06/17）",
+                              ],
+                           farm_number: 100,
                            temperature: { Aug: 23.0, Sep: 18.5, Oct: 16.8 },
                            humidity: { Aug: 23.0, Sep: 18.5, Oct: 16.8 },
                            precipitation: { Aug: 110, Sep: 160, Oct: 80 },
@@ -182,19 +201,6 @@ export default function Rental(props) {
       getAvailFarms();
    }, []);
    */
-
-   // Data displayed in ResultDetail.
-   const [resultDetailData, setResultDetailData] = useState([]);
-
-   function updateRentalDetail(data) {
-      setResultDetailData(data);
-   }
-
-   // Update ResultDetail when TableCell is clicked.
-   useEffect(()  => {
-      console.log("ResultDetailData is updated.");
-      console.log(resultDetailData.name);
-   }, [resultDetailData]);
 
    /*
    async function getAvailFarms() {
@@ -235,7 +241,6 @@ export default function Rental(props) {
       <div class='rental-container'>
          <Flex direction="row">
             <Result displayData={resultData} />
-            <ResultDetail />
          </Flex>
       </div>
    );
