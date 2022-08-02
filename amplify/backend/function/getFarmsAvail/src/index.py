065e6c71578fa963590b33e13acc435235b2e7ed
TakahitoMotoki <t.motoki.1030@gmail.com>
2022-08-02 21:47:22 +0900

    220802

--- amplify/backend/function/getFarmsAvail/src/index.py
+++ amplify/backend/function/getFarmsAvail/src/index.py
@@ -8,11 +8,43 @@ farmTable = dynamoDB.Table('Farm-j2gchw5g4zhf5nhp4oq4w3sgaq-devi')
 farmComTable = dynamoDB.Table('FarmCom-j2gchw5g4zhf5nhp4oq4w3sgaq-devi')
 
 def handler(event, context):
-    output = { "farm": {}, "farmcom": {}}
+    output = {}
+    availFarmList = {}
     farmComIDList = []
+ 
+    # Farmテーブルのスキャン 
+    try:
+        queryData_Farm = farmTable.scan()
+    except Exception as e:
+        return {
+            'statusCode': 200,
+            'headers': {
+                'Access-Control-Allow-Headers': '*',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+            },
+            'body': json.dumps('Error in getFarmAvail')
+        }
+       
+    for data in queryData_Farm["Items"]:
+        # FarmがAvailableだった場合、1.farmComIDListへの追加、2.FarmComIDごとのAvail Farmsリスト作成
+        if data["is_available"]:
+            farmComID = data["farmcom_id"]
+            # 1.farmComIDListへの追加
+            if data["farmcom_id"] not in farmComIDList: 
+                farmComIDList.append(farmComID)
+                availFarmList[farmComID] = {}
+            
+            #2.FarmComIDごとのavailFarmList作成
+            availFarmList[farmComID][data["id"]] = {
+                "id": str(data["id"]),
+                "name": str(data["name"]),
+                "fee": str(1500), # <== TEST ==>
+                "last_used": str("2022-12-17") # <== TEST ==>
+            }
 
     try:
-        queryData = farmTable.scan()
+        queryData_FarmCom = farmComTable.scan()
     except Exception as e:
         return {
             'statusCode': 200,
@@ -24,30 +56,21 @@ def handler(event, context):
             'body': json.dumps('Error in getFarmAvail')
         }
 
-    for data in queryData["Items"]:
-        if data["is_available"]:
-            output["farm"][data["id"]] = {
+    for data in queryData_FarmCom["Items"]:
+        farmComID = data["id"]
+        if farmComID in farmComIDList:
+            output[farmComID] = {
                 "name": str(data["name"]),
-                "length": str(data["length"]),
-                "width": str(data["width"]),
-                "area": str(data["area"]),
-                "farmcom_id": str(data["farmcom_id"])
+                "temperature": str(data["temperature"]),
+                "humidity": str(data["humidity"]),
+                "precipitation": str(data["precipitation"]),
+                "vol_of_sunshine": str(data["vol_of_sunshine"]),
+                "country": str(data["country"]),
+                "city": str(data["city"]),
+                "address": str(data["address"]),
+                "is_organic": str(data["is_organic"]),
+                "avail_farms": availFarmList[farmComID]
             }
-            
-            if data["farmcom_id"] not in farmComIDList: 
-                farmComIDList.append(data["farmcom_id"])
-
-    for farmComID in farmComIDList:
-        queryData = farmComTable.query(
-            KeyConditionExpression = Key('id').eq(farmComID) 
-        )
-    
-        output["farmcom"][farmComID] = {
-            "temperature": str(queryData["Items"][0]["temperature"]),
-            "humidity": str(queryData["Items"][0]["humidity"]),
-            "precipitation": str(queryData["Items"][0]["precipitation"]),
-            "vol_of_sunshine": str(queryData["Items"][0]["vol_of_sunshine"])
-        }  
 
     return {
         'statusCode': 200,
--- src/components/Rental.js
+++ src/components/Rental.js
@@ -1,4 +1,4 @@
-import { Button, Card, Flex, Heading, Image, SelectField, Text } from "@aws-amplify/ui-react";
+import { Button, Card, Divider, Flex, Heading, Image, SelectField, Text } from "@aws-amplify/ui-react";
 import { API } from "aws-amplify";
 import { useState, useEffect } from 'react';
 
@@ -6,6 +6,10 @@ import './Rental.css';
 
 const apiName = 'api0c080238';
 const apiPath = '/farms/avail';
+const initHeader = {
+   headers: {
+   }
+}
 
 function CustomSelectField(props) {
    return(
@@ -76,49 +80,62 @@ function Search(props) {
 
 function Record(props) {
    const organic_converter = {0: "不可", 1: "可"}
+
    return(
       <Card
          width="100%"
-         border="solid gray"
+         border="solid #888"
          borderRadius="10px"
          margin="20px"
       >
-         <Flex direction="row" alignItems="flex-start">
-            <Image
-               alt="Road to milford sound"
-               src="/road-to-milford-new-zealand-800w.jpg"
-               width="30%"
-            />
-            <Flex
-               direction="column"
-               alignItems="flex-start"
-               width="70%"
-            >
-               <Heading level={5}>
-                  {props.displayData.name}
-               </Heading>
-
-               <Text>
-                  有機栽培: {organic_converter[props.displayData.is_organic]}
-               </Text>
-
-               <Text>
-                  空き農園： {props.displayData.avail_number} / {props.displayData.farm_number}
-               </Text>
-               <CustomSelectField
-                  title="農園の選択"
-                  margin="0"
-                  options={props.displayData.avail_farms}
+         <Flex direction="column">
+            <Flex direction="row" alignItems="flex-start">
+               <Image
+                  alt="Road to milford sound"
+                  src="/road-to-milford-new-zealand-800w.jpg"
+                  width="30%"
                />
-
-               <Button
-                  size='small'
-                  margin="20px 0 0 0"
-                  width="100%"
+               <Flex
+                  direction="column"
+                  alignItems="flex-start"
+                  height="200px"
+                  width="70%"
                >
-                  レンタル
-               </Button>
+                  <Heading level={5}>
+                     {props.displayData.name}
+                  </Heading>
+
+                  <Text>
+                     有機栽培: {organic_converter[props.displayData.is_organic]}
+                  </Text>
+
+                  <Text>
+                     空き農園： {props.displayData.avail_number} / {props.displayData.farm_number}
+                  </Text>
+
+               </Flex>
             </Flex>
+
+            <Flex direction="column">
+               <Divider border="0.3px solid #888" borderRadius="0.3px" />
+               <Flex direction="row">
+                  <Text>農園名</Text>
+                  <Text>コスト 円/月</Text>
+                  <Text>最終利用</Text>
+               </Flex>
+            </Flex>
+
+            {props.displayData.avail_farms.map(avail_farm => (
+               <Flex direction="column">
+                  <Divider border="0.3px solid #888" borderRadius="0.3px" />
+                  <Flex direction="row">
+                     <Text>{avail_farm.name}</Text>
+                     <Text>{avail_farm.fee}</Text>
+                     <Text>{avail_farm.last_used}</Text>
+                  </Flex>
+               </Flex>
+            ))}
+            
          </Flex>
       </Card>
    )
@@ -153,94 +170,102 @@ function Result(props) {
 }
 
 export default function Rental(props) {
+
    // Data displayed in Result.
+   const [tableData, setTableData] = useState([]);
    const resultData = [
                         {
                            name: "埼玉 XX農園",
-                           length: 5.0,
-                           width: 0.7,
-                           area: 3.5,
                            is_organic: 1,
-                           avail_number: 3,
-                           avail_farms:
-                              [
-                                 "農場003 （1,500円/月、最終利用: 2021/12/17）",
-                                 "農場007 （2,500円/月、最終利用: 2022/06/17）",
-                                 "農場019 （2,500円/月、最終利用: 2022/06/17）",
-                              ],
-                           farm_number: 50,
-                           temperature: { Aug: 23.0, Sep: 18.5, Oct: 16.8 },
                            temperature: { Aug: 23.0, Sep: 18.5, Oct: 16.8 },
                            humidity: { Aug: 23.0, Sep: 18.5, Oct: 16.8 },
                            precipitation: { Aug: 110, Sep: 160, Oct: 80 },
-                           vol_of_sunshine: { Aug: 120, Sep: 80, Oct: 60 }
+                           vol_of_sunshine: { Aug: 120, Sep: 80, Oct: 60 },
+                           avail_vegitables:
+                              [
+                                 "エダマメ"
+                              ],
+                           farm_number: 50,
+                           avail_number: 3,
+                           avail_farms:
+                              [
+                                 { id: "", name: "農園003", fee: 1500, last_used: "2021-12-17", area: 5.0 },
+                                 { id: "", name: "農園007", fee: 2500, last_used: "2022-06-17", area: 5.0 },
+                                 { id: "", name: "農園019", fee: 3500, last_used: "2021-06-17", area: 7.0 }
+                              ]
                         },
                         {
                            name: "埼玉 YY農園",
-                           length: 5.0,
-                           width: 0.7,
-                           area: 3.5,
                            is_organic: 0,
-                           avail_number: 3,
-                           avail_farms:
-                              [
-                                 "農場003 （1,500円/月、最終利用: 2021/12/17）",
-                                 "農場007 （2,500円/月、最終利用: 2022/06/17）",
-                                 "農場019 （2,500円/月、最終利用: 2022/06/17）",
-                              ],
-                           farm_number: 100,
                            temperature: { Aug: 23.0, Sep: 18.5, Oct: 16.8 },
                            humidity: { Aug: 23.0, Sep: 18.5, Oct: 16.8 },
                            precipitation: { Aug: 110, Sep: 160, Oct: 80 },
-                           vol_of_sunshine: { Aug: 120, Sep: 80, Oct: 60 }
+                           vol_of_sunshine: { Aug: 120, Sep: 80, Oct: 60 },
+                           avail_vegitables:
+                              [
+                                 "エダマメ",
+                                 "ビーツ"
+                              ],
+                           farm_number: 100,
+                           avail_number: 3,
+                           avail_farms:
+                              [
+                                 { id: "", name: "農園003", fee: 1500, last_used: "2021-12-17", area: 5.0 },
+                                 { id: "", name: "農園007", fee: 2500, last_used: "2022-06-17", area: 5.0 },
+                                 { id: "", name: "農園019", fee: 3500, last_used: "2021-06-17", area: 7.0 }
+                              ]
                         }
                      ];
-   /*
+
    // Get available farms when the page is loaded.
    useEffect(()  => {
       getAvailFarms();
    }, []);
-   */
-
-   /*
+   
    async function getAvailFarms() {
-      API.get(apiName, apiPath, {}).then(response => {
+      const data = [];
+
+      API.get(apiName, apiPath, initHeader).then(response => {
          console.log("+=+=+ in getAvailFarms +=+=+");
-         let respFarm = response["farm"];
-         let respFarmCom = response["farmcom"];
+         console.log(response);
 
          // FarmComからデータを抽出しtableDataにセットする
-         for (let key in respFarm) {
-            // Farm: farmcom_id と一致するFarmComを探す
-            if (respFarm[key]["farmcom_id"] in respFarmCom) {
-               let farmComKey = respFarm[key]["farmcom_id"];
-               // FarmCom情報も含めてtableDataにセット
-               tableData.push(
-                  {
-                     name: respFarm["name"],
-                     length: respFarm["length"],
-                     width: respFarm["width"],
-                     area: respFarm["area"],
-                     temperature: respFarmCom[farmComKey]["temperature"],
-                     humidity: respFarmCom[farmComKey]["humidity"],
-                     precipitation: respFarmCom[farmComKey]["precipitation"],
-                     vol_of_sunshine: respFarmCom[farmComKey]["vol_of_sunshine"]
-                  }
-               );
+         for (let key in response) {
+            const record = response[key]
+
+            // avail_farms用の配列を作成
+            const avail_farms = [];
+            for (let farmKey in response[key]["avail_farms"]) {
+               avail_farms.push(response[key]["avail_farms"][farmKey]);
             }
+               
+            data.push(
+               {
+                  name: record["name"],
+                  is_organic: record["is_organic"],
+                  temperature: record["temperature"],
+                  humidity: record["humidity"],
+                  precipitation: record["precipitation"],
+                  vol_of_sunshine: record["vol_of_sunshine"],
+                  avail_vegitables: ["エダマメ"],
+                  farm_number: 100,
+                  avail_number: 3,
+                  avail_farms: avail_farms
+               }
+            );
          }
+         setTableData(data);
       }).catch(err => {
         console.log(err);
       });
       console.log("Called API");
       console.log(tableData);
    }
-   */
 
    return(
       <div class='rental-container'>
          <Flex direction="row">
-            <Result displayData={resultData} />
+            <Result displayData={tableData} />
          </Flex>
       </div>
    );
